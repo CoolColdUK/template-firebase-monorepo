6a855a45f05f05d12edf85df4f8d66fb13b919e5
CoolColdUK <coolcold@gmail.com>
2024-01-31 00:28:30 +0000

    before initialise vite

---
+++ packages/web/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA()],
+  server: {port: 3000},
+  define: {
+    // 'process.env': process.env,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
